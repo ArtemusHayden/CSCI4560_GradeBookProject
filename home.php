e11239298304b855bf240907e9f416673f29a0c7
Artemus Hayden <grayson2005hayden@gmail.com>
2025-10-09 13:34:32 -0500

    Update home.php

--- home.php
+++ home.php
@@ -7,10 +7,25 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+<div class="navigation">
+    <div class="nav-left">
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+        <a href="home.php">Home</a>
+        <a href="about.php">About</a>
+    </div>
+    <div class="nav-right">
+        <p>SimplyGrade</p>
+    </div>
+</div>
+
+<div class="welcome-container">
     <h2>Welcome to SimplyGrade!</h2>
-    <h3>Click below to login or register to and view your grades!</h3>
-
-    <a href="login.php">Login</a>
-    <a href="register.php">Register</a>
-
+    <h3>Click below to login or register and view your grades!</h3>
+    <div class="welcome-buttons">
+        <a href="login.php" class="btn">Login</a>
+        <a href="register.php" class="btn">Register</a>
+    </div>
+</div>
 </body>
