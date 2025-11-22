f0f21eac41e51d94447fdae2d4645144597b0b91
Artemus Hayden <grayson2005hayden@gmail.com>
2025-11-22 13:37:35 -0600

    Update home.php

--- home.php
+++ home.php
@@ -7,25 +7,25 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class="navigation">
+        <div class="nav-left">
+                <a href="login.php">Login</a>
+                <a href="register.php">Register</a>
+                <a href="home.php">Home</a>
+                <a href="about.php">About</a>
+                <a href="logout.php">Logout</a>
+        </div>
+        <div class="nav-right">
+            <p>SimplyGrade</p>
+        </div>
+    </div>
 
-<div class="navigation">
-    <div class="nav-left">
-        <a href="login.php">Login</a>
-        <a href="register.php">Register</a>
-        <a href="home.php">Home</a>
-        <a href="about.php">About</a>
+    <div class="welcome-container">
+        <h2>Welcome to SimplyGrade!</h2>
+        <h3>Click below to login or register and view your grades!</h3>
+        <div class="welcome-buttons">
+            <a href="login.php" class="btn">Login</a>
+            <a href="register.php" class="btn">Register</a>
+        </div>
     </div>
-    <div class="nav-right">
-        <p>SimplyGrade</p>
-    </div>
-</div>
-
-<div class="welcome-container">
-    <h2>Welcome to SimplyGrade!</h2>
-    <h3>Click below to login or register and view your grades!</h3>
-    <div class="welcome-buttons">
-        <a href="login.php" class="btn">Login</a>
-        <a href="register.php" class="btn">Register</a>
-    </div>
-</div>
 </body>
