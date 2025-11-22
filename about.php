b7ef0ef3dd055ef3fa94b06ce9a4e5e24c6a5a4a
Artemus Hayden <grayson2005hayden@gmail.com>
2025-11-22 13:37:05 -0600

    Update about.php

--- about.php
+++ about.php
@@ -7,26 +7,25 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<div class="navigation">
-    <div class="nav-left">
-        <a href="login.php">Login</a>
-        <a href="register.php">Register</a>
-        <a href="home.php">Home</a>
-        <a href="about.php">About</a>
+    <div class="navigation">
+        <div class="nav-left">
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="logout.php">Logout</a>
+        </div>
+        <div class="nav-right">
+            <p>SimplyGrade</p>
+        </div>
     </div>
-    <div class="nav-right">
-        <p>SimplyGrade</p>
+
+    <div class="about-section">
+        <h3>What is SimplyGrade?</h3>
+        <p>SimplyGrade is a lightweight, easy-to-use online gradebook designed to help students and teachers manage courses, 
+            track grades, and stay organized. Our platform allows you to view assignments, monitor progress, and stay up to 
+            date with class performance.</p>
+
+        <p>For information on how to register your organization, please email us at admin@simplygrade.com</p>
     </div>
-</div>
-
-<div class="about-section">
-    <h3>What is SimplyGrade?</h3>
-    <p>SimplyGrade is a lightweight, easy-to-use online gradebook designed to help students and teachers manage courses, 
-        track grades, and stay organized. Our platform allows you to view assignments, monitor progress, and stay up to 
-        date with class performance.</p>
-
-    <p>For information on how to register your organization, please email us at admin@simplygrade.com</p>
-</div>
-
 </body>
